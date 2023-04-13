b11a2eba91cc9e2b26c758c6f2a83b781f7a8da4
Haile-08 <haile4cmd@gmail.com>
2023-04-13 10:15:24 +0300

    Render animation on the canvas

--- client/src/pages/Boad.jsx
+++ client/src/pages/Boad.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import "../App.css";
 
+let x = 0;
 function Boad() {
   const canvaRef = useRef(null);
-
   useEffect(() => {
-    const canvas = canvaRef.current;
-    const ctx = canvas.getContext("2d");
+    const render = () => {
+      const canvas = canvaRef.current;
+      const ctx = canvas.getContext("2d");
 
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.stroke();
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.arc(x, 75, 50, 0, 2 * Math.PI);
+      ctx.stroke();
+      x++;
+      console.log("render");
+      requestAnimationFrame(render);
+    };
+    render();
   }, []);
   return <canvas className="canvas" ref={canvaRef} height="500" width="700" />;
 }
