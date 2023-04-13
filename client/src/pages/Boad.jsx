e6a3cc166b30351018d2f431c92537af9dd0cb07
Haile-08 <haile4cmd@gmail.com>
2023-04-13 10:06:55 +0300

    Setup canva context in react

--- client/src/pages/Boad.jsx
+++ client/src/pages/Boad.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "../App.css";
 
 function Boad() {
-  return <canvas className="canvas" height="500" width="500" />;
+  const canvaRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvaRef.current;
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  }, []);
+  return <canvas className="canvas" ref={canvaRef} height="500" width="700" />;
 }
 
 export default Boad;
